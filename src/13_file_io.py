acc24288d71a6e32fbd95d47ecfb8d9bca920673
Miley Wright <dishaliekwright@yahoo.com>
2020-07-04 13:56:57 -0400

    file created

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
-
+file = open('src/foo.txt', 'r')
+print(file.read())
+file.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+newFile = open('src/bar.txt', 'w')
+newFile.write('Today\'s July 5th. It\'s been good to have the week off. I\'ve had a chance to get ahead in some work.')
